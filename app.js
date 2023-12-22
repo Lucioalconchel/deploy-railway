a7ac4d56603784adb2d3051a9ca152a77cbcb3d4
Lucio <lucioalconchelsj@gmail.com>
2023-12-22 01:47:04 -0300

    m

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 import express from "express";
 import bodyParser from "body-parser";
+import {PrismaClient} from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -15,17 +18,19 @@ app.get('/', (req,res)=>{
     res.send("hola usuario")
 
 })
-/*
-app.post('/inicio', (req,res)=>{
-    const {username} = req.body;
-    res.send({msg: `Bienvenido ${username}`});
-})
-*/
+app.get('/nuevo', async (req,res)=>{
+    newUser();
+    res.redirect('/')
+});
 app.get('/usuario', async(req,res)=>{
-    const usuario = await Prisma.usuario.create({
-        data:{
-            nombre: "Lucio"
-        }
-    });
-    res.json(usuario);
+    const usuarios = await prisma.usuario.findMany();
+    res.json(usuarios)
 })
+async function newUser(){
+    const user = await prisma.usuario.create({
+        data:{
+            nombre : "lucio"
+        }
+    })
+    return user;
+}
--- dist/app.dev.js
+++ dist/app.dev.js
@@ -4,8 +4,11 @@ var _express = _interopRequireDefault(require("express"));
 
 var _bodyParser = _interopRequireDefault(require("body-parser"));
 
+var _client = require("@prisma/client");
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
+var prisma = new _client.PrismaClient();
 var app = (0, _express["default"])();
 var PORT = process.env.PORT || 3000;
 app.use(_bodyParser["default"].urlencoded({
@@ -18,34 +21,63 @@ app.listen(PORT, function () {
 app.get('/', function (req, res) {
   res.send("hola usuario");
 });
-/*
-app.post('/inicio', (req,res)=>{
-    const {username} = req.body;
-    res.send({msg: `Bienvenido ${username}`});
-})
-*/
-
-app.get('/usuario', function _callee(req, res) {
-  var usuario;
+app.get('/nuevo', function _callee(req, res) {
   return regeneratorRuntime.async(function _callee$(_context) {
     while (1) {
       switch (_context.prev = _context.next) {
         case 0:
-          _context.next = 2;
-          return regeneratorRuntime.awrap(Prisma.usuario.create({
-            data: {
-              nombre: "Lucio"
-            }
-          }));
+          newUser();
+          res.redirect('/');
 
         case 2:
-          usuario = _context.sent;
-          res.json(usuario);
-
-        case 4:
         case "end":
           return _context.stop();
       }
     }
   });
 });
+app.get('/usuario', function _callee2(req, res) {
+  var usuarios;
+  return regeneratorRuntime.async(function _callee2$(_context2) {
+    while (1) {
+      switch (_context2.prev = _context2.next) {
+        case 0:
+          _context2.next = 2;
+          return regeneratorRuntime.awrap(prisma.usuario.findMany());
+
+        case 2:
+          usuarios = _context2.sent;
+          res.json(usuarios);
+
+        case 4:
+        case "end":
+          return _context2.stop();
+      }
+    }
+  });
+});
+
+function newUser() {
+  var user;
+  return regeneratorRuntime.async(function newUser$(_context3) {
+    while (1) {
+      switch (_context3.prev = _context3.next) {
+        case 0:
+          _context3.next = 2;
+          return regeneratorRuntime.awrap(prisma.usuario.create({
+            data: {
+              nombre: "lucio"
+            }
+          }));
+
+        case 2:
+          user = _context3.sent;
+          return _context3.abrupt("return", user);
+
+        case 4:
+        case "end":
+          return _context3.stop();
+      }
+    }
+  });
+}
